70cf1b471e3a5bb76f80400e8d3e56116f9756df
Katsu <evergonuaa@gmail.com>
2019-08-13 00:38:19 +0800

    add mdw

--- app/centaur/centaur.go
+++ app/centaur/centaur.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"os"
+
+	"github.com/Katsusan/centaur/internal/commands"
+	"github.com/Katsusan/centaur/internal/config"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+const (
+	version = "1.0"
+)
+
 func main() {
+	app := cli.NewApp()
+	app.Name = "centaur"
+	app.Version = version
+	app.Usage = "say anything you want about IT career"
+	app.EnableBashCompletion = true
+	app.Flags = config.GlobalFlags
+
+	app.Commands = []cli.Command{
+		commands.StartCommand,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 
 }
